964a312cbbd299fd55a90548353bbd8fddd953e0
vaish44 <daradevaishnavi2000@gmail.com>
2022-08-25 09:58:18 +0530

    use your logic

--- primeNumber.ts
+++ primeNumber.ts
@@ -1,4 +1,14 @@
 function isPrime(num:number):boolean{
     let isPrime = false;
+    if(num==2){
+        isPrime=true;
+        return isPrime;
+    }
+    for(let i=0 ;i<num/2 ; i++)
+
+        if(num%i==0){
+            isPrime=false;
+            break;
+        }
     return isPrime;
 }
